e2c6d4cd8c2e7c731f4a1ddecbda37324cdc6cc3
izaazJoJ <isaacoliveirazuza28@gmail.com>
2025-09-26 21:53:08 -0300

    feat: adicionar interface de cálculo e enum de formas

---
+++ src/main/java/br/edu/fatec/factory/ShapeType.java
@@ -0,0 +1,15 @@
+package br.edu.fatec.factory;
+
+public enum ShapeType {
+    SQUARE,
+    RECTANGLE,
+    CIRCLE,
+    TRIANGLE,
+    PARALLELOGRAM,
+    TRAPEZO,
+    HEXAGON,
+    DIAMOND,
+    CUBE
+}
+
+
